257ec76e1a1a4a779b7ea0e432f5728be1c5d739
Kajal Jha <kajal832jha@gmail.com>
2020-07-16 12:05:52 +0530

    Initial commit

---
+++ palindrome_checker.py
@@ -0,0 +1,28 @@
+string = input('Enter a string:')
+
+
+def checker1(string):
+  string_len = len(string)
+  flag = True
+  for i in range(string_len):
+    if string[i] != string[string_len-1-i]:
+       flag = False
+       print('No palindrome')
+       break
+  if flag:
+    print('Palindrome')
+
+
+def checker2(string):
+  string_len = len(string)
+  for i in range(string_len):
+    if string[i] != string[string_len-1-i]:
+      print('No palindrome')
+      break
+  else:
+    print('Palindrome')
+
+checker1(string)
+checker2(string)
+
+checker2('madamimadam')
