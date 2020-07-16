1528e5154ec216f530dea26237615dc94c4a1b6c
Kajal Jha <kajal832jha@gmail.com>
2020-07-16 12:27:46 +0530

    Implemented checker 4

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,4 +1,4 @@
-string = input('Enter a string:')
+# string = input('Enter a string:')
 
 
 def checker1(string):
@@ -32,7 +32,32 @@ def checker3(string):
   else:
     print('Palindrome')
 
-checker1(string)
-checker2(string)
-checker3(string)
-checker2('madamimadam')
+def checker4(string):
+  string_len = len(string)
+  i, j = 0, string_len-1
+
+  while i <= j:
+    if string[i] != string[j]:
+      print('No palindrome')
+      break
+    i += 1
+    j -= 1
+  else:
+    print('Palindrome')
+
+if __name__ == '__main__':
+  palindromes = ['madam', 'madamiamadam', 'eye', 'mam', 'kanak', 'malayalam']
+  non_palindromes = ['bag', 'principal', 'teacher']
+  
+  for string in palindromes:
+    print(string, ':'); checker1(string)
+    print(string, ':'); checker2(string)
+    print(string, ':'); checker3(string)
+    print(string, ':'); checker4(string)
+  
+  for string in non_palindromes:
+    print(string, ':'); checker1(string)
+    print(string, ':'); checker2(string)
+    print(string, ':'); checker3(string)
+    print(string, ':'); checker4(string)
+  
